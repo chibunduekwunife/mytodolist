39357d4126a0cb09b9cabb4e134bcc77c9c12a01
chibunduekwunife <chibunduekwunife@gmail.com>
2025-01-27 21:48:36 -0500

    lksd

--- my-todo-list/src/components/TaskWindow.jsx
+++ my-todo-list/src/components/TaskWindow.jsx
@@ -9,7 +9,7 @@ function TaskWindow({ tasks,  deleteTask }) {
 
     return (
         <div>
-            <h2 className='text-3xl font-bold underline bg-red'>Tasks:</h2>
+            <h2 className='text-3xl'>Tasks:</h2>
             {tasks.length > 0 ? (
                 tasks.map((task, index) => (
                     <div key={index}>
